2794439dbe8cde794a1a7e511a6aca630c46b04d
Anthony Ramirez <anthonyj0127@gmail.com>
2025-12-12 14:40:48 -0600

    Finished adding ai to business page

--- src/lib/test.mjs
+++
@@ -1,9 +0,0 @@
-import { getBusinessDescriptionFromSite } from "./openai-query.ts";
-
-const output = await getBusinessDescriptionFromSite(
-  "https://rioplexbizx.com",
-  "100 E Nolana Ave #130, McAllen, TX 78504",
-  "RioPlex Business Exchange"
-);
-
-console.log(output);
